9993641b8f34107ccc5d7e2fcdc84cb33f36dc24
Waruni-cpu <warunisandareka963@gmail.com>
2023-09-02 20:08:21 +0530

    New welcome screen

--- lib/welcome.dart
+++ lib/welcome.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:lottie/lottie.dart';
 import 'package:pawpal/register.dart';
 
@@ -6,27 +7,32 @@ class WelcomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: null, // Set the app bar to null to remove it
       body: Container(
-        color: Colors.white, // Change the background color here
+        color: Color(hexColor('#FEEBEB')), // Change the background color here
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.start, // Align children to the top
           children: [
-            Image.asset(
-              'assets/images/wave.svg', // Replace with your image path
-              width: 200.0,
-              height: 200.0,
+            Container(
+              margin: EdgeInsets.zero,
+              alignment: Alignment.topCenter, // Align the child to the top
+              child: SvgPicture.asset(
+                'assets/images/wave2.svg', // Replace with your image path
+                width: 500.0,
+                height: 170.0,
+              ),
             ),
             Text(
-              "PawPal",
+              "BmiCal",
               style: TextStyle(
                 fontSize: 37.0,
                 fontWeight: FontWeight.bold,
                 fontFamily: 'poor',
-                color: Colors.amber,
+                color: Colors.black54,
               ),
             ),
             Lottie.asset(
-              'assets/animations/pet_hug.json', // Replace with the path to your Lottie file
+              'assets/animations/girl.json', // Replace with the path to your Lottie file
               width: 1300,
               height: 300,
             ),
@@ -34,10 +40,10 @@ class WelcomeScreen extends StatelessWidget {
             ElevatedButton(
               child: Text(
                 "Get Started",
-                style: TextStyle(fontSize: 18.0, fontFamily: 'poor', fontWeight: FontWeight.bold),
+                style: TextStyle(fontSize: 19.0, fontFamily: 'poor', fontWeight: FontWeight.bold),
               ),
               style: ElevatedButton.styleFrom(
-                primary: Colors.amber, // Change the button background color here
+                primary: Colors.amberAccent, // Change the button background color here
                 onPrimary: Colors.black,
                 padding: EdgeInsets.symmetric(horizontal: 52.0, vertical: 12.0),
                 shape: RoundedRectangleBorder(
@@ -57,4 +63,11 @@ class WelcomeScreen extends StatelessWidget {
       ),
     );
   }
+}
+
+int hexColor(String color){
+  String newColor = '0xff' + color;
+  newColor = newColor.replaceAll('#', '');
+  int finalColor = int.parse(newColor);
+  return finalColor;
 }
